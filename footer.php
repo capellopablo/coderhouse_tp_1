49d599f8cdf0053fc8b24e89f53eef0994a04852
Pablo Capello <capellopablo@ccdesigns.com.ar>
2016-07-09 12:16:00 -0300

    first

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+			<div class="site-info">
+				<a href="#">Este es mi Custom Footer, en la clase de Andres</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
